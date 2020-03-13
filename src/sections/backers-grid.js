06920c6abc882bca2007b4333663f1e3dc457ab0
Owner <vtsoneva@gmail.com>
2020-03-13 14:39:13 +0100

    modify tiers css

--- src/sections/backers-grid.js
+++ src/sections/backers-grid.js
@@ -2,18 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 import Tier from './tier';
 
+import bat from '../assets/bat.svg';
+import rocket from '../assets/rocket.svg';
+import beer from '../assets/beer.svg';
+
 const BackersGridContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 30%);
-  grid-column-gap: var(--big);
+  grid-template-columns: repeat(3, 27%);
+  grid-column-gap: var(--huge);
 `
 
 const Backers = (props) => {
   return (
     <BackersGridContainer>
-      <Tier title="Donate once" text='Make a custom one time or recurring contribution to support this collective.' buttonTitle='donate' />
-      <Tier title="Donate once a month"  text='Support us with a monthly donation and help us continue our activities.' buttonTitle='donate' />
-      <Tier title="Unknown tier 3" text='Become a sponsor and get your logo on our README with a link to your site.' buttonTitle='donate' />
+      <Tier title="Donate once" text='Make a custom one time or recurring contribution to support this collective.' buttonTitle='donate' icon={bat} />
+      <Tier title="Donate once a month"  text='Support us with a monthly donation and help us continue our activities.' buttonTitle='donate' icon={rocket} />
+      <Tier title="Unknown tier 3" text='Become a sponsor and get your logo on our README with a link to your site.' buttonTitle='donate' icon={beer} />
     </BackersGridContainer>
   )
 }
--- src/sections/tier.js
+++ src/sections/tier.js
@@ -3,16 +3,31 @@ import styled from 'styled-components';
 import Button from '../components/button';
 
 const TierContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1.5fr 0.5fr;
+`
+
+const Heading = styled.div`
+  display: flex;
+
+  img {
+    margin-right: var(--tiny);
+  }
+
   h4 {
     color: var(--red);
-    margin-bottom: 0;
+    align-self: center;
   }
 `
 
 const Tier = (props) => {
   return (
     <TierContainer>
-      <h4>{props.title}</h4>
+      <Heading>
+        <img src={props.icon} alt={props.title} />
+        <h4>{props.title}</h4>
+      </Heading>
       <p>{props.text}</p>
       <Button title={props.buttonTitle} />
     </TierContainer>

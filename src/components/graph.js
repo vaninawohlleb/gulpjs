df1808e547dc98155c87eb29575ec88dd14d8b04
Owner <vtsoneva@gmail.com>
2020-03-22 14:26:11 +0200

    animation svg

--- src/components/graph.js
+++ src/components/graph.js
@@ -1,16 +1,56 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { ReactComponent as GulpVisual } from '../assets/gulp-visual.svg';
 
-import graph from '../assets/gulp-graph.svg';
+const fill = keyframes`
+  0% {
+    stroke-dasharray: 100 400;
+    stroke-dashoffset: 200;
+  }
 
-// const GraphContainer = styled.div`
-//   font-size: 12px;
-//   line-height: 1;
-// `
+  50% {
+    stroke: var(--red);
+    stroke-dashoffset: 0;
+  }
 
+  100% {
+    stroke-dasharray: 100 400;
+    stroke-dashoffset: -200;
+  }
+`
+
+const opacity = keyframes`
+  0%, 50%, 75% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`
+
+const StyledGulp = styled(GulpVisual)` 
+  width: 100%;
+  height: 100%;
+
+  .line-up-anim {
+    animation: ${fill} infinite 4s linear;
+    
+    &.second {
+      animation: ${opacity} 2s linear forwards, ${fill} infinite 4s linear 1.3s;
+    }
+  }
+
+  .line-down-anim {
+    animation: ${opacity} 5s linear forwards, ${fill} infinite 4s linear 2.5s;
+
+    &.second {
+      animation-delay: .4s;
+    }
+  }
+` 
 const Graph = (props) => {
   return (
-    <img src={graph} alt="gulp graph" />
+    <StyledGulp />
   )
 }
 
